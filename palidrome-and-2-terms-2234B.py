db963c64c762b57525e4b338d2f47bac18e5ef20
Anusha Syeda <anoshasyeda1999@gmail.com>
2026-06-09 16:20:47 +0500

    Add Solution for palidrome-and-2-terms-2234B.py

--- palidrome-and-2-terms-2234B.py
+++ palidrome-and-2-terms-2234B.py
@@ -1,12 +1,29 @@
 import sys;
 
 input = sys.stdin.readline
-def findTerms(n):
-    multipleOf12 = 12;
-    
 
-    
-    
+def isPalindrom(num):
+    numStr = str(num);
+    if numStr == numStr[::-1]:
+        return True;
+    return False;
+
+def findTerms(n):
+    if n == 10:
+        print(-1);
+        return;
+
+    rem = n % 12;
+    if rem != 10 and isPalindrom(rem):
+        b = n - rem;
+        print(rem, b);
+        return;
+    elif rem == 10:
+        a = 12 + rem;
+        b = n - a;
+        print(a, b);
+        return;
+
 if __name__ == "__main__":
     noOfTestCases = int(input());
     for _ in range(noOfTestCases):
